4c7de5c7154549e917a82fcfe4a9f688ee8ae65d
gwiederhecker <>
2021-09-02 16:07:46 -0300

    Update leticia_lib.py

--- leticia_lib.py
+++ leticia_lib.py
@@ -5,7 +5,17 @@ import os
 def time_stamp():
     return time.strftime("%Y-%m-%d_%H-%M-%S")
 
+def folder(my_folder):
+    if not os.path.exists(my_folder):
+        os.makedirs(my_folder)
 
+
+
+#def scope_avg(scope, channel):    
+#    scope.measurement.initiate()    
+#    time.sleep(timebase)
+#    vmean = scope.channels[channel].measurement.fetch_waveform_measurement("voltage_average")
+#    return vmean
 
 
 if __name__=='__main__':
