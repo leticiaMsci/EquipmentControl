658f95c81029010bac590a64e7611dc8ef50a304
gwiederhecker <>
2021-09-03 13:42:12 -0300

    Update leticia_lib.py

--- leticia_lib.py
+++ leticia_lib.py
@@ -2,8 +2,13 @@
 import time
 import os
 
-def time_stamp():
-    return time.strftime("%Y-%m-%d_%H-%M-%S")
+def time_stamp(precision_minute = True, precision_second = False):
+    if not precision_minute:
+        return time.strftime("%Y-%m-%d_%H")
+    elif not precision_second:
+        return time.strftime("%Y-%m-%d-%H-%M")
+    else:
+        return time.strftime("%Y-%m-%d-%H-%M-%S")
 
 def folder(my_folder):
     if not os.path.exists(my_folder):
