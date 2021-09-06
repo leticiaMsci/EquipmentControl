a327de7684508dc14c50dcb6ca7011c775577691
gwiederhecker <>
2021-09-06 17:17:13 -0300

    Update mzi_ruler.py
    just started

--- mzi_ruler.py
+++ mzi_ruler.py
@@ -1,10 +1,19 @@
 #%%
 from sympy import symbols, sqrt, diff, lambdify
 from scipy import constants, interpolate, signal, optimize
+import pyLPD.MLtools as mlt
+import pandas as pd
+import numpy as np
 
 c = constants.c
 π = constants.pi
 ħ=constants.hbar
+
+nist_path = 'C:/Users/lpd/Documents/Leticia/DFS/EquipmentControl/hcn_nist.csv'
+nist= pd.read_csv(nist_path, sep = ',')
+nist.head()
+
+
 #%%
 B1, B2, B3 = 0.6961663, 0.4079426, 0.8974794
 C1, C2, C3 = 68.4043, 116.2414, 9896.161 # in nm
@@ -35,3 +44,32 @@ D2_mzi = -(2*π*β2/β1)*(1/β1/L)**2 # in THz
 D3_mzi = (4*π**2/L**3/β1**5)*(3*β2**2-β1*β3) # in THz
 
 D1_mzi = 1/β1/L # in THz
+
+
+if __name__=='__main__':
+    import glob, os
+    path ="C:\\Users\\lpd\\Documents\\Leticia\\DFS\\2021-09-02_OpticalTransmission\\2021-09-06-16_OpticalTransmission_BareSphere-238umDiameter_Tunics-Laser-5nms-Pol1_C-Band"#os.getcwd()
+    data_folder=""#'2021-09-03-12_OpticalTransmission_BareSphere-238umDiameter_Tunics-Laser-5nms-Pol1_C-Band'#'Data_Folder'
+    extension = '.parq'
+
+    flist = sorted(glob.glob(os.path.join(path, data_folder, "*"+extension)), key = os.path.getmtime)
+    N = 0
+    fname = flist[N]
+
+    df = pd.read_parquet(fname)
+    
+    ylower, df['yupper_cav'] = mlt.envPeak(df.cav.values, delta=0.01,  smooth=0.1, sg_order=1)
+    df['cav_n'] = df.cav/(df['yupper_cav'])
+
+    df['ylower_mzi'], df['yupper_mzi'] = mlt.envPeak(df.mzi.values, delta=0.01, sg_order=1) # Finding lower and upper envelope
+    df['mzi_n'] = (df.mzi-df['ylower_mzi'])/(df['yupper_mzi']-df['ylower_mzi']) # Normalizing data
+    df['mzi_s'] = mlt.savitzky_golay(df.mzi_n.values, window_size = 7, order = 2)
+
+    delta = 0.3
+    ind_max, maxtab, ind_min, mintab = mlt.peakdet(df.mzi_n.values, delta)
+    ind_peaks = np.sort(np.concatenate((ind_min, ind_max), axis=0))
+
+    df['hcn_n'] = (df.hcn - df.hcn.min())/(df.hcn.max() - df.hcn.min())
+
+    ind_max_hcn, maxtab_hcn, ind_min_hcn, mintab_hcn = mlt.peakdet(df.hcn_n.values, 0.2)
+
