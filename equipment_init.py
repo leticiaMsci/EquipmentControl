a8d0c0c45a438d0fb3c2cc12389e5a614b1f6c19
gwiederhecker <leticiamscience@gmail.com>
2021-11-15 16:21:28 -0300

    kwargs to define which equipment to initialize

--- equipment_init.py
+++ equipment_init.py
@@ -12,8 +12,8 @@ import leticia_lib as llb
 
 #equipment id
 daq_port = 'Dev1/ao1'
-att_in_id = 'ASRL13::INSTR'
-att_r_id = 'ASRL8::INSTR'
+att_in_id = 'ASRL5::INSTR'
+att_r_id = 'ASRL13::INSTR'
 tunics_ip = 'yetula.ifi.unicamp.br'
 sigen_id = 'USB0::0x0957::0x2B07::MY52701124::INSTR'
 scope_id = 'TCPIP0::nano-osc-ag9254a::hislip0::INSTR'
@@ -32,57 +32,72 @@ ch_hcn=3
 #other details
 pm_delta_dB = 18.33
 
-def init_equip():
-    #attenuators
-    att_out = VOA_lib.VOA(daq_port, os.path.join(equip_control_path, VOA_calib_path))
-    att_in = att_lib.Att(resource_str = att_in_id)
-    att_r = att_lib.Att(resource_str = att_r_id)
-
-    #other equipment
-    pxa = PXA_lib.N9030A()
-    tunics = tunics_lib.T100R(ip=tunics_ip)
-    tunics.connect()
-    scope = ivi.agilent.agilentDSOX92504A(scope_id, prefer_pyvisa = True)
-    sigen = sigen_lib.Sigen(sigen_id = sigen_id)
-
-    osa1 = aq63XX.AQ63XX()
-    osa1.ConnectOSA(isgpib = False, iseth = True, ethip = osa1_id, ipuser = 'anonymous', ippass='123456')
-    osa1.osa.write('AUTO OFFSET OFF')
-    osa1.InitOSA(print_bool=False)
-
-    osa2 = aq63XX.AQ63XX()
-    osa2.ConnectOSA(isgpib = False, iseth = True, ethip = osa2_id, ipuser = '', ippass='')
-    osa2.osa.write('AUTO OFFSET OFF')
-    osa2.InitOSA(print_bool=False)
-
-    pm = thorlabs_pm_lib.PM200(pm_id)
-
-    edfa = EDFA_lib.KeopsysEDFA(gpib_port=edfa_gpib_port)
-
-
-    print("All Equipment Initialized Successfully")
+def init_equip(**kwargs):#pxa_bool = True, tunics_bool = True, scope_bool = True, sigen_bool = True,
+               # osa1_bool = True, osa2_bool = True, pm_bool = True, edfa_bool = True,
+               # att_out_bool = True, att_in_bool = True, att_r_bool = True):
 
     equip = {
-        'att_out' : att_out,
-        'att_in' : att_in,
-        'att_r' : att_r,
-
-        'pxa' : pxa,
-        'tunics' : tunics,
-        'scope' : scope,
-        'sigen' : sigen,
-
         'scope_ch_r' : ch_reflection,
         'scope_ch_t' : ch_transmission,
         'scope_ch_mzi' : ch_mzi,
         "scope_ch_hcn" : ch_hcn,
-
-        'osa1' : osa1,
-        'osa2' : osa2,
-
-        'pm':pm,
-        'edfa':edfa
     }
+    #attenuators
+    if kwargs.get('att_out') is True: 
+        att_out = VOA_lib.VOA(daq_port, os.path.join(equip_control_path, VOA_calib_path))
+        equip['att_out'] = att_out
+
+    if kwargs.get('att_in') is True:
+        att_in = att_lib.Att(resource_str = att_in_id)
+        equip['att_in'] = att_in
+
+    if kwargs.get('att_r') is True:
+        att_r = att_lib.Att(resource_str = att_r_id)
+        equip['att_r'] = att_r
+
+    #other equipment
+    if kwargs.get('pxa') is True:
+        pxa = PXA_lib.N9030A()
+        equip['pxa'] = pxa
+
+    if kwargs.get('tunics') is True:
+        tunics = tunics_lib.T100R(ip=tunics_ip)
+        tunics.connect()
+        equip['tunics'] = tunics
+
+    if kwargs.get('scope') is True:
+        scope = ivi.agilent.agilentDSOX92504A(scope_id, prefer_pyvisa = True)
+        equip['scope'] = scope
+    
+    if kwargs.get('sigen') is True:
+        sigen = sigen_lib.Sigen(sigen_id = sigen_id)
+        equip['sigen'] = sigen
+
+    if kwargs.get('osa1') is True:
+        osa1 = aq63XX.AQ63XX()
+        osa1.ConnectOSA(isgpib = False, iseth = True, ethip = osa1_id, ipuser = 'anonymous', ippass='123456')
+        osa1.osa.write('AUTO OFFSET OFF')
+        osa1.InitOSA(print_bool=False)
+        equip['osa1'] = osa1
+
+
+    if kwargs.get('osa2') is True:
+        osa2 = aq63XX.AQ63XX()
+        osa2.ConnectOSA(isgpib = True, iseth = False, address=6, ethip = osa2_id, ipuser = '', ippass='')
+        osa2.osa.write('AUTO OFFSET OFF')
+        osa2.InitOSA(print_bool=False)
+        equip['osa2'] = osa2
+
+    if kwargs.get('pm') is True:
+        pm = thorlabs_pm_lib.PM200(pm_id)
+        equip['pm'] = pm
+
+    if kwargs.get('edfa') is True:
+        edfa = EDFA_lib.KeopsysEDFA(gpib_port=edfa_gpib_port)
+        equip['edfa'] = edfa
+
+
+    print("All Equipment Initialized Successfully")
 
     return equip
 
@@ -92,5 +107,6 @@ if __name__=='__main__':
     rm = visa.ResourceManager()
     print(rm.list_resources())
 
-    init_equip()
+    equip = init_equip(osa2 = True)
+    
 # %%
